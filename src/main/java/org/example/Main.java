c7905816b7d4d9b47c8f63deded3994a49d33e4b
vuntila <valeriu.untila@yahoo.com>
2024-01-29 16:36:45 +0200

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,37 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Steps to synchronize Git Hub with project on Intelej Ideia:");
+        System.out.println("1. Create repository and copy repository link from Code button dropdown");
+        System.out.println("2. Go to Intelej menu>>Git>>Manage Remotes>>put link in Git remotes popup");
+        System.out.println("3. Select Git>>Fech after this wait successful message");
+        System.out.println("4. Click on Git>>Pull");
+        System.out.println("5. Now is needed to do Git>>commit");
+        System.out.println("6. In the next step, select Git>>push ");
+        System.out.println("Git commands used:");
+        System.out.println("1. git config --global user.name <user.name>");
+        System.out.println("2. git config --global user.email <user.email>");
+        System.out.println("3. git add or git add --all <add all files for future commit>");
+        System.out.println("4. git add . or git add <file name> or git add --all <Add all files for future commit>");
+        System.out.println("5. git commit -m <Write a comment>");
+        System.out.println("6. git brunch <name branch>  Create a new branch");
+        System.out.println("7. git checkout <new branch name> Switch to another branch");
+        System.out.println("8. git checkout -b <new branch name> Create a new branch and switching to that branch");
+        System.out.println("9. git checkout -f <branch>  Force switch to a branch but the chenges will be lost");
+        System.out.println("10. git checkout  Force switch to last commit on the same branch but the chenges will be lost");
+        System.out.println("11. git stash  To save the unfinished changes that are not part of the commit and make it possible to switch to another branch");
+        System.out.println("12. git stash pop  To revert the changes saved on the branch");
+        System.out.println("13. git merge <branch>  concatenation branch with master");
+        System.out.println("14. git branch -d <branch>  Delete the branch only if concatenation branch with master");
+        System.out.println("15. git branch -D <branch>  Delete the branch but all commits will be deleted");
+        System.out.println("16. Git status    to see actual status, the last changes");
+        System.out.println("17. git clone     to clone project from repository to local masine, on project");
+        System.out.println("18. git push origin <branch name>  For renewing the branch in the repository");
+        System.out.println("19. git pull  To pull from the remote repository all the changes that were made on brunch");
+        System.out.println("20. git revert   To bring the code to the point where you started");
+        System.out.println("21. git show --pretty=fuller    To view a commit");
+        System.out.println("22. git show --pretty=fuller    To view a commit");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
